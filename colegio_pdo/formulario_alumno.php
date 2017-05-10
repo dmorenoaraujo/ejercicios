cf828c3f9de2d464aceec62f2cb634dbe3f84a90
Daniel Moreno <dmorenoaraujo@gmail.com>
2017-05-10 13:20:36 +0200

    copia de colegio con conexión mysql PDO

---
+++ colegio_pdo/formulario_alumno.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+    </head>
+    <body>
+        <div class="container">
+            
+            <h1>FORMULARIO ALUMNOS</h2>
+
+            <form action="nuevo_alumno.php" method="post" enctype="multipart/form-data">
+                <label for="nombre">Nombre</label>
+                <input class="form-control" type="text" name="nombrecito1" id="nombrecito2" placeholder="NOMBRE">
+                <label for="apellidos">Apellidos</label>
+                <input class="form-control" type="text" name="apellidos" id="apellidos" placeholder="APELLIDOS">
+                <label for="fecha">Fecha de Nacimiento</label>
+                <input class="form-control" type="text" name="fecha_nacimiento" id="fecha" placeholder="dd-mm-aaaa">
+                
+                <label for="cursos">Curso:</label>
+
+                <?php
+                //$conn = new mysqli("localhost", "root", "P15!1754123m", "colegio");
+                //mysqli_set_charset($conn, 'utf8'); // con esta línea habilitamos los caracteres especiales para la conexión actual
+                $db = new PDO('mysql:host=localhost;dbname=colegio;charset=utf8','root','P15!1754123m');
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $sql = "SELECT * FROM curso";
+                
+                //$result = $conn->query($sql);
+                
+                try {
+                    $st = $db->prepare($sql);
+                    $st->execute();
+                } catch (PDOException $e) {
+                    echo $e->getMessage();
+                    return false;
+                }
+                
+                
+                
+                
+                echo '<select id="cursos" name="curso_id">
+                        <option selected disabled>Elija</option>'
+                ;
+                            while ($opcion=$st->fetch(PDO::FETCH_ASSOC)){
+                                echo '<option value="'.$opcion['id'].'">'.$opcion['nombre'].'</option>';
+                            }
+                echo   '</select>';
+                //$conn->close();
+
+                ?>
+                <br>
+                <label for="nota">Nota</label>
+                <input class="form-control" type="text" name="nota" id="nota" placeholder="Nota">
+                <label for="ficheros">Añadir imagenes</label>
+                <input class="form-control" type="file" name="adjuntos" id="ficheros">
+                <input type="submit" value="Enviar">    
+            </form>
+            <button class="btn btn-info" onclick="location.href='lista_alumno.php'" type="button">Listado de alumnos</button>
+        </div>
+
+
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.9.2/i18n/jquery.ui.datepicker-es.min.js"></script>
+        <script>
+            $( function() {
+                $( "#fecha" ).datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    showAnim: 'bounce', // con esta línea añadimos animación al datepicker
+                    
+                });
+                // con este comando tambien se puede hacer la animación de datepicker $( "#fecha" ).datepicker( "option", "showAnim", "bounce" );
+            } );
+        </script>
+        
+        
+    </body>
+</html>
---
+++ colegio_pdo/lista_alumno.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <style>
+            h1{
+                padding-top: 5rem;
+            }
+            table thead td{
+                text-transform: capitalize;
+            }
+
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>ALUMNOS DEL COLEGIO</h1>
+
+            <?php
+            $db = new PDO('mysql:host=localhost;dbname=colegio;charset=utf8','root','P15!1754123m');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            //mysqli_set_charset($conn, 'utf8');
+            
+            $sql = "SELECT * FROM alumno";
+
+            //$result = $conn->query($sql);
+            
+            try {
+                $st = $db->prepare($sql);
+                $st->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                return false;
+            }
+            
+            $primerafila= $st->fetch(PDO::FETCH_ASSOC);
+            
+            //$nombrecolumnas=array_keys($primerafila);
+            
+            echo '<table id="mitabla" class="display" width="100%" cellspacing="0">';
+            echo "<thead>";
+            echo "<tr>";
+            foreach ($primerafila as $clave => $nombrecolumna){
+                //var_dump ($clave);
+                //var_dump ($nombrecolumna);
+                if ($clave == 'curso_id'){
+                    echo "<td>" . str_replace ("curso_id","Nº Curso",$clave) . "</td>";
+                } else if ($clave == 'nota'){
+                    echo "<td>" . $clave . "</td>";
+                } else if ($clave == 'fecha_nacimiento' || 'fecha_alta'){
+                    echo "<td style='text-align: center'>" . str_replace ("fecha_","Fecha de ",$clave) . "</td>";
+                } else {
+                    echo "<td>" . $clave . "</td>";
+                }
+            }
+            echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+
+            listar($primerafila);
+              //echo "<td>". $elementosprimerafila . "</td>";            
+
+
+            while ($fila=$st->fetch(PDO::FETCH_ASSOC)){
+                listar($fila);
+            }
+            echo "</tbody>";
+            echo "</table>";
+
+            //$conn->close();
+
+            
+            
+            function listar($item){
+                echo "<tr>";
+                echo "<td>".$item['id']."</td>";
+                echo "<td>".$item['curso_id']."</td>";
+                echo "<td>".$item['nombre']."</td>";
+                echo "<td>".$item['apellidos']."</td>";
+                echo "<td>".$item['dni']."</td>";
+                echo "<td style='text-align:right'>". $nota = number_format($item['nota'],2,',','.') . "</td>"; 
+                echo "<td  style='text-align: center'>". date ("d-m-Y", strtotime ($item['fecha_nacimiento']))."</td>";
+                if ($item['matriculado']==1){
+                echo "<td>Si</td>";
+                }else{
+                echo "<td>No</td>";
+                };
+                echo "<td><img src='/var/www/html/colegio_pdo/imagenes/".$item['adjunto']."'></td>";
+                echo "<td>".$item['fecha_alta']."</td>";
+                echo "</tr>";                
+            }
+            ?>
+
+            <button class="btn btn-info" onclick="location.href='formulario_alumno.php'" type="button">Formulario de alta</button>
+
+
+        </div>
+
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/colreorder/1.3.3/js/dataTables.colReorder.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js"></script>
+        <script src="https://cdn.datatables.net/plug-ins/1.10.15/sorting/datetime-moment.js"></script>
+        <script>
+            $(document).ready(function () {
+                $.fn.dataTable.moment ('DD-MM-YYYY'),
+                $('#mitabla').dataTable({
+                    "language": {
+                        "url": "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+                    },
+                    "lengthMenu": [50, 10, 25],
+                    "colReorder": true
+                });
+            });
+        </script>
+    </body>
+</html>
